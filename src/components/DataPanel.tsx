ddce2b9019fc2b5bf486db8d79216b70f6291bb9
LovroG05 <lovro@govekar.net>
2026-05-31 11:53:46 +0200

    gitisit

--- src/components/DataPanel.tsx
+++ src/components/DataPanel.tsx
@@ -4,7 +4,7 @@ import IconBar from "./IconBar";
 
 export default function DataPanel({ setRefreshKey }: { setRefreshKey: Dispatch<SetStateAction<number>> }) {
   return (
-    <div style={{  height: '100vh', background: '#0d1931', overflowY: 'auto', minWidth: "280px", flex: "1"}}>
+    <div style={{  height: '100vh', background: '#0d1931', overflowY: 'auto', minWidth: "280px", maxWidth: "480px", flex: "1"}}>
         <IconBar />
         <ButtonBox setRefreshKey={setRefreshKey} />
 
