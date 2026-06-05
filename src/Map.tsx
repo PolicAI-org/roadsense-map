3b9b1930019d22b6d59813130e7467baaa5d7c90
blaz <blaz.bocek@gmail.com>
2026-06-05 20:51:32 +0200

    DEV-112 Dodal reloadRoadData dependency v Map.tsx zaradi ci.

--- src/Map.tsx
+++ src/Map.tsx
@@ -117,7 +117,7 @@ export default function Map({ refreshKey, visibleFileIds, boundsToFit}: {
     })
 
     return () => map.remove()
-  }, [])
+  }, [reloadRoadData])
 
   useEffect(() => {
     reloadRoadData()
